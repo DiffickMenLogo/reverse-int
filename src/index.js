ef65f5002a1db86ac38740392f206b10c7400ea8
DiffickMenLogo <gh.sdfs@mail.ru>
2022-02-21 11:30:00 +0300

    feat: add module reverse

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let mas = n.toString().split('');
+    if(mas[0] == '-'){
+        mas.shift();
+    }
+    result = mas.reverse().join('');
+    return result;
 }
